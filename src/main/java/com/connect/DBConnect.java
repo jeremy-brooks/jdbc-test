8e9b97bc22ce9e92859e2923577789e4b0b49d77
jeremy-brooks <jeremybrooks321@gmail.com>
2016-01-04 18:20:27 +0000

    Changed the dependency for jdbc driver to an openly available one.

--- src/main/java/com/connect/DBConnect.java
+++ src/main/java/com/connect/DBConnect.java
@@ -1,47 +1,49 @@
 package com.connect;
 
-import java.sql.*;
+import net.sourceforge.jtds.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by jeremybrooks on 27/07/15.
  */
 public class DBConnect {
 
-    static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    static final String URL = "jdbc:sqlserver://{server}{port}";
+    static final String URL = "jdbc:jtds:sqlserver://{hostname}:1433/{dbname}";
     static final String USER = "{username}";
     static final String PASS = "{password}";
 
     Connection connection = null;
     Statement statement = null;
+    Driver jdbcDriver = new Driver();
 
     public DBConnect(){
         try {
             // register connection
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Class.forName(jdbcDriver.getClass().getName());
 
             // open connection
             this.connection = DriverManager.getConnection(URL, USER, PASS);
 
             // execute query
-            this.statement = connection.createStatement();
-            String sql = "SELECT [last_sync_time] FROM [swdata].[dbo].[replication_status]";
-            ResultSet rs = statement.executeQuery(sql);
-
-            // extract data from result set
-            while (rs.next()){
-                // retrieve by column
-                System.out.print(rs.getDate("last_sync_time"));
-                System.out.print("T" + rs.getTime("last_sync_time"));
-            }
-
-            rs.close();
-            statement.close();
+//            this.statement = connection.createStatement();
+//            String sql = "SELECT ...";
+//            ResultSet rs = statement.executeQuery(sql);
+//
+//            // extract data from result set
+//            while (rs.next()){
+//                // retrieve by column
+//            }
+//            rs.close();
+  //          statement.close();
             connection.close();
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
